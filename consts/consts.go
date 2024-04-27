40b11002638d41f73a189d3cdce630defdff406b
maanyamanjunath <110166485+maanyamanjunath@users.noreply.github.com>
2024-04-27 19:07:56 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Maanya's Hyperchain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Maanya"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "MM"
 )
 
 var ID ids.ID
